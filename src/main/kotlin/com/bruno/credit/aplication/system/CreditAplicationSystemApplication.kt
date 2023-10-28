70fc942d5d938d1810f08b0d67799a9c0ad47a25
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2023-10-28 02:04:12 -0300

    Initial commit

---
+++ src/main/kotlin/com/bruno/credit/aplication/system/CreditAplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.bruno.credit.aplication.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAplicationSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/bruno/credit/aplication/system/CreditAplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bruno.credit.aplication.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
